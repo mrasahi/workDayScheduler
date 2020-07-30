76c3f7569a3fa02832bd18e8eb374ae4e7c96e1c
mrasahi <makotorasahi@gmail.com>
2020-07-29 21:21:50 -0700

    class bg loop works

--- assets/js/planner.js
+++ assets/js/planner.js
@@ -1,3 +1,22 @@
-let currentTime = moment().format(HH)
+// Current time hour in military time to compare easier
+let currentTime = parseInt(moment().format('HH'))
 
-console.log(currenttime)
+console.log(currentTime)
+
+// Add class to each time block
+function hourBg() {
+    for (i = 9; i <= 17; i++) {
+        // Resets classes first
+        $(`#${i}`).removeClass('past present future')
+        // Check hour with currentTime, then add class bg
+        if (parseInt($(`#${i}`).attr('id')) < currentTime) {
+            $(`#${i}`).addClass('past')
+        } else if (parseInt($(`#${i}`).attr('id')) === currentTime) {
+            $(`#${i}`).addClass('present')
+        } else {
+            $(`#${i}`).addClass('future')
+        }
+    }
+}
+
+hourBg()
